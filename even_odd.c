d8ad709c061d14944a5dd6899e7c1a445c0f7d85
Abhishek jadhav <109812422+Abhishekjadhavv@users.noreply.github.com>
2022-07-25 18:04:54 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,15 @@
+// Write a program to check whether a given number is an even number or an odd
+// number.
+
+#include<stdio.h>
+
+int main(){
+    int num = 46;
+
+    if(num % 2 == 0){
+     printf("even number");
+    }else{
+        printf("odd number");
+    }
+    return 0;
+}
